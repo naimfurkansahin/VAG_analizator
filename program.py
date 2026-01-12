3b5e6b6b5b4e9cfd3fc82877a717da68a74d318c
naimfurkansahin <naimfurkansahin@gmail.com>
2026-01-12 15:08:44 +0300

    sahibinden.com dan gerçek verileri çekip grafiğe döktüm.

--- program.py
+++ program.py
@@ -3,20 +3,20 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-# --- AYARLAR ---
+# ayar
 sns.set_theme(style="whitegrid", context="talk", font_scale=0.9)
 
-# --- VERİ OKUMA ---
+# veri read
 try:
     df = pd.read_csv('arabalar.csv')
 except FileNotFoundError:
     print("CSV bulunamadı!")
     exit()
 
-# --- GÖRSELLEŞTİRME ---
+# görselleştirme
 fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 16), dpi=150) # Boyutu biraz uzattım
 
-# 1. GRAFİK
+# 1. grafik
 sns.barplot(x='Marka', y='Fiyat', data=df, palette='viridis', hue='Marka', legend=False, errorbar=None, ax=ax1)
 ax1.set_title('Marka Bazlı Ortalama Fiyatlar', fontweight='bold', pad=20)
 ax1.set_xlabel('') # Üstteki grafikte 'Marka' yazmasına gerek yok, zaten belli. Sildik, yer açıldı.
@@ -24,7 +24,7 @@ ax1.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.1f} M₺'
 for i in ax1.containers:
     ax1.bar_label(i, fmt='%.0f TL', padding=3, fontsize=10)
 
-# 2. GRAFİK
+# 2. grafik
 sns.scatterplot(data=df, x='KM', y='Fiyat', hue='Marka', s=250, palette='deep', alpha=0.8, ax=ax2)
 ax2.legend(bbox_to_anchor=(1.02, 1), loc='upper left', borderaxespad=0) # Lejant dışarıda
 
@@ -32,7 +32,7 @@ ax2.set_title('Fırsat Matrisi (Sol Alt = Kelepir)', fontweight='bold', color='d
 ax2.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.1f} M₺'.format(x/1000000)))
 ax2.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '{:,.0f} bin'.format(x/1000)))
 
-# Etiketler
+# etiketler
 for i in range(df.shape[0]):
     ax2.text(
         df.KM[i] + 2000, # Biraz daha sağa
@@ -46,11 +46,11 @@ for i in range(df.shape[0]):
 
 sns.despine(left=True, bottom=True)
 
-# DÜZENLEME: Boşlukları mükemmel ayarla
-plt.tight_layout(rect=[0, 0, 1, 0.96]) # Tepeden boşluk
-plt.subplots_adjust(hspace=0.3) # İki grafik arasını aç
+# düzenleme
+plt.tight_layout(rect=[0, 0, 1, 0.96]) # tepe boşluğu
+plt.subplots_adjust(hspace=0.3) # iki grafik arasını aç
 
-# --- KAYDETME (Bu klasöre resim olarak indirir) ---
+# png olarak Kaydetme
 plt.savefig('vag_analiz_raporu.png', bbox_inches='tight', dpi=300)
 print("Rapor 'vag_analiz_raporu.png' olarak kaydedildi!")
 
